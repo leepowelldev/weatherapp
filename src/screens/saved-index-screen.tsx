bf4395ad386556fc1c14f8b646b86df76f289459
Lee Powell <powell.lee@gmail.com>
2023-07-19 18:59:54 +0100

    Link saved index to detail page

--- src/screens/saved-index-screen.tsx
+++ src/screens/saved-index-screen.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Button, Text } from 'react-native';
 import { Layout } from '../components';
+import { useFocusEffect } from '@react-navigation/native';
+import { getSavedLocationsFromStorage } from '../utils/saved-locations-storage';
+import { SavedIndexScreenProps } from '../router';
 
-export function SavedIndexScreen(): JSX.Element {
+export function SavedIndexScreen({
+  navigation,
+}: SavedIndexScreenProps): JSX.Element {
+  const [savedLocations, setSavedLocations] = useState<Set<string>>(new Set());
+
+  function handlePress(location: string) {
+    navigation.navigate('Detail', { location });
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      getSavedLocationsFromStorage().then((locations) => {
+        if (locations === null) {
+          return setSavedLocations(new Set());
+        }
+        setSavedLocations(locations);
+      });
+    }, [])
+  );
+
   return (
     <Layout>
       <Text>Saved Index</Text>
+      {Array.from(savedLocations).map((location) => (
+        <Button
+          key={location}
+          title={location}
+          onPress={() => handlePress(location)}
+        />
+      ))}
     </Layout>
   );
 }
