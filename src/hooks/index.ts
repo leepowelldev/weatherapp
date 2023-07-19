20d0ab10a8ad3d1d5d86fb60c0cb80c01a9693a4
Lee Powell <powell.lee@gmail.com>
2023-07-19 18:10:14 +0100

    Hooks for getting current and forecast weather

---
+++ src/hooks/index.ts
@@ -0,0 +1,2 @@
+export * from './use-current-weather';
+export * from './use-forecast-weather';
---
+++ src/hooks/use-forecast-weather.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useReducer } from 'react';
+import { WeatherApiService } from '../domain/services/weatherapi';
+import { env } from '../env';
+
+type State = {
+  data: Awaited<ReturnType<WeatherApiService['getForecastByLocation']>> | null;
+  isLoading: boolean;
+  error: Error | null;
+};
+
+type Action =
+  | { type: 'DATA'; value: State['data'] }
+  | { type: 'IS_LOADING'; value: State['isLoading'] }
+  | { type: 'ERROR'; value: State['error'] };
+
+function reducer(state: State, action: Action): State {
+  const { type, value } = action;
+
+  switch (type) {
+    case 'DATA': {
+      return {
+        ...state,
+        data: value,
+        isLoading: false,
+        error: null,
+      };
+    }
+    case 'IS_LOADING': {
+      return {
+        ...state,
+        isLoading: value,
+      };
+    }
+    case 'ERROR': {
+      return {
+        ...state,
+        data: null,
+        isLoading: false,
+        error: value,
+      };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${type}`);
+    }
+  }
+}
+
+const initialState = {
+  data: null,
+  isLoading: false,
+  error: null,
+};
+
+export function useForecastWeather(
+  location: string | null,
+  options: { days: number } = { days: 5 }
+): [State['data'], { isLoading: State['isLoading']; error: State['error'] }] {
+  const [{ data, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(() => {
+    (async () => {
+      if (!location) {
+        dispatch({ type: 'DATA', value: null });
+        return;
+      }
+
+      const service = new WeatherApiService({ key: env.WEATHERAPI_API_KEY });
+
+      try {
+        dispatch({ type: 'IS_LOADING', value: true });
+        const response = await service.getForecastByLocation({
+          location,
+          days: options.days,
+        });
+        dispatch({ type: 'DATA', value: response });
+      } catch (err) {
+        if (err instanceof Error) {
+          dispatch({ type: 'ERROR', value: err });
+        } else {
+          throw err;
+        }
+      }
+    })();
+  }, [location, options.days]);
+
+  return [data, { isLoading, error }];
+}
